70c0977deae3867d1fb90d9e754400b2665a4349
JakubPedryc <133998600+JakubPedryc@users.noreply.github.com>
2023-05-23 17:41:30 +0200

    Update Main.java

--- src/main/java/efs.task.oop/Main.java
+++ src/main/java/efs.task.oop/Main.java
@@ -1,5 +1,8 @@
 package efs.task.oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         List<Villager> villagersList = new ArrayList<>();
